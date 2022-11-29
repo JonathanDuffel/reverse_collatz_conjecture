247a9494c6824d0c5a29baa929a637171415af15
JonathanDuffel <82407815+JonathanDuffel@users.noreply.github.com>
2022-11-29 16:21:48 -0600

    Update gamma.py

--- gamma.py
+++ gamma.py
@@ -1,25 +1,25 @@
 def gamma(n, s):
-    reverseCollatz = [n]
+    gamma_class = [n]
     
-    def gamma_one(n, mod):
+    def gamma_one(n, desired_mod):                          # Returns reverse collatz result
         k = 1
-        result = n
-        while result % 6 not in mod or k == 1:
-            result = (((2**k)*n)-1)//3
-            k += 1
+        result = n                                          # Keep n unchanged
+        while result % 6 not in desired_mod or result == n: # Gamma class must not have repeating elements
+            result = (((2**k)*n)-1)//3                      # Reverse collatz equation
+            k += 1                                          # Incriment k until the result is a desired mod6
         return result
     
-    def gamma_any(n, s):
-        threemod = False
-        while not threemod:
+    def gamma_any(n, s):                                    # Returns gamma class beggining at n with s steps
+        threemodsix = False
+        while not threemodsix:                              # Runs repeatedly until n is 3mod6
             if s == 1:
-                n = gamma_one(n, [3])
-                threemod = True
+                n = gamma_one(n, [3])                       # Finds final gamma step with n % 6 = 3
+                threemodsix = True
             else:
-                n = gamma_one(n, [1, 5])
+                n = gamma_one(n, [1,5])                     # Finds next gamma step with n % 6 = 1 or 5
             s -= 1
-            reverseCollatz.append(n)
-        return reverseCollatz
+            gamma_class.append(n)                           # Add n to gamma class
+        return gamma_class
         
     return gamma_any(n, s)
     
