c801e1837a5519e520622acea4978e67304ef1e7
JonathanDuffel <82407815+JonathanDuffel@users.noreply.github.com>
2022-12-04 00:51:07 -0600

    Update gamma_one.py

--- gamma_one.py
+++ gamma_one.py
@@ -2,6 +2,6 @@ def gamma_one(number):
     k = 1
     result = number
     while result % 6 != 1 and result % 6 != 5 or result == number:
-        result = ((2**k) * result) - 1) // 3
+        result = (((2**k) * result) - 1) // 3
         k += 1
     return result
